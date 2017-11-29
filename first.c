fba626b829f13005416191e43c801c99f76f0854
therubincodes <33031607+therubincodes@users.noreply.github.com>
2017-11-29 19:41:09 +0100

    Update first.c

--- first.c
+++ first.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(){
+  while(1){
+    printf("Welcome");
+    sleep();
+    quest1();
+  }  
+}
+void quest1(){
+      char anw[4];
+      printf("Do you have an account?!");
+      scanf("%c", &anw);
+      if(){
+        
+      }
+}
